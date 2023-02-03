6d11d40c7373e77e12645d96c886178a078d5eaf
Rishabh-Singh-Codes <rishabhsinghcodes@gmail.com>
2023-02-03 22:04:40 +0530

    added await

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 export const getUser = async (req, res) => {
     try {
         const { id } = req.params;
-        const user = User.findById(id);
+        const user = await User.findById(id);
         res.status(200).json(user);
     } catch (error) {
         res.status(404).json({ message: error.message });
